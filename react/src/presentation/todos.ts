4089313f18397fda521ce846b81e35ce3e33dd8a
JairoPerte <jairopertecarras@gmail.com>
2025-05-20 11:58:36 +0200

    pruebas

---
+++ react/src/presentation/todos.ts
@@ -0,0 +1,19 @@
+import type { Todo } from "./todotype";
+
+export const data: Todo[] = [
+  {
+    id: 1,
+    title: "Titulo 1",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "Titulo 2",
+    completed: false,
+  },
+  {
+    id: 3,
+    title: "Titulo 3",
+    completed: false,
+  },
+];
--- react/vite.config.ts
+++ react/vite.config.ts
@@ -1,19 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path'
-import { fileURLToPath } from 'url'
-import tailwindcss from "@tailwindcss/vite"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { fileURLToPath } from "url";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 // Necesario para ESM, simula __dirname
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      "@": path.resolve(__dirname, "src"),
     },
   },
-})
+});
