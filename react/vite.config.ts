15082d32764209b58e98199359877610785ac12d
JairoPerte <jairopertecarras@gmail.com>
2025-05-20 11:28:09 +0200

    Preparado el tailwindcss

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 // Necesario para ESM, simula __dirname
@@ -9,7 +10,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
