caa2d6de4d9802e7be2dabf26412fd4e7ee65773
JairoPerte <jairopertecarras@gmail.com>
2025-05-23 09:58:58 +0200

    Proyecto migrado a next.js

--- react/src/config/env.tsx
+++
@@ -1,4 +0,0 @@
-export const BASE_URL = "http://localhost:80";
-
-export const API_URL = `${BASE_URL}/api/v1`;
-export const MEDIA_URL = `${BASE_URL}/media`;
--- react/src/presentation/App.tsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-  return (
-    <div className="min-h-screen bg-red-500 text-white p-8 text-2xl">
-      <p>dsahdjashd</p>¡Tailwind está funcionando!
-    </div>
-  );
-}
