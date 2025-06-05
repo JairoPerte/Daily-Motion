c81fabd5eb9efd1b5abb04edec6f4fbe8ea93747
JairoPerte <jairopertecarras@gmail.com>
2025-06-05 08:16:37 +0200

    Arreglada la subida de archivos en el controler de settings

--- symfony/src/User/Infrastructure/Controller/UserSettings/UserSettingsController.php
+++ symfony/src/User/Infrastructure/Controller/UserSettings/UserSettingsController.php
@@ -2,42 +2,56 @@
 
 namespace App\User\Infrastructure\Controller\UserSettings;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Shared\Infrastructure\Context\FileContext;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use App\Authentication\Infrastructure\Context\AuthContext;
-use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
+use App\Shared\Infrastructure\FileProcessor\FileProcessor;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\User\Application\UseCase\UserSettings\UserSettingsCommand;
 use App\User\Application\UseCase\UserSettings\UserSettingsHandler;
+use App\User\Infrastructure\Controller\UserSettings\UserSettingsRequest;
 use App\User\Infrastructure\Controller\UserSettings\UserSettingsResponse;
+use Throwable;
 
 class UserSettingsController extends AbstractController
 {
     public function __construct(
         private AuthContext $authContext,
         private UserSettingsHandler $handler,
-        private FileContext $fileContext
+        private FileContext $fileContext,
+        private FileProcessor $fileProcessor
     ) {}
 
-    #[Route(path: "/v1/user/settings", name: "v1_user_settings", methods: ["PUT"])]
+    #[Route(path: "/v1/user/settings", name: "v1_user_settings", methods: ["POST"])]
     public function index(
-        #[MapRequestPayload] UserSettingsRequest $request
+        Request $request,
     ): JsonResponse {
+        $requestObj = new UserSettingsRequest(
+            name: $request->request->get("name"),
+            usertag: $request->request->get("usertag"),
+            newPassword: $request->request->get("newPassword"),
+            oldPassword: $request->request->get("oldPassword")
+        );
+
         $imgProfile = $this->fileContext->getUserProfileImg();
 
         $imgURL = null;
         if ($imgProfile) {
-            //Debería de guardar la img de alguna manera buscar
+            $imgURL = $this->fileProcessor->uploadProfileIcon(
+                imgProfile: $imgProfile,
+                userId: $this->authContext->getUserId()
+            );
         }
 
         $command = new UserSettingsCommand(
             id: $this->authContext->getUserId(),
             sessionId: $this->authContext->getSessionId(),
-            name: $request->name,
-            usertag: $request->usertag,
-            newPassword: $request->newPassword,
-            oldPassword: $request->oldPassword,
+            name: $requestObj->name,
+            usertag: $requestObj->usertag,
+            newPassword: $requestObj->newPassword,
+            oldPassword: $requestObj->oldPassword,
             img: $imgURL
         );
 
